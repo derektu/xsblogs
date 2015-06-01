9b9ce058ab491cc3d3a0c6376f39aacaf4977ef5
Derek Tu <derektu123@gmail.com>
2015-06-01 19:55:28 +0800

    add fb-default-image plugin

---
+++ wp-content/plugins/fb-default-image/fb-default-image.php
@@ -0,0 +1,46 @@
+<?php
+/*
+    Copyright (C) 2015  SysJust Co.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/*
+Plugin Name: FBDefaultImage
+Plugin URI: https://github.com/derektu/fb-default-image
+Description: Provide default image when post to facebook
+Version: 1.0.0
+Author: Derek Tu
+Author URI: https://github.com/derektu
+*/
+
+/*
+ * TODO: currently there is no management UI to set the default image.
+ */
+
+/**
+ * Add site-specific Open Graph protocol customizations to the Facebook plugin
+ *
+ * @param array $meta_tags an associative array of Open Graph protocol properties with keys expressed as full IRIs
+ * @return array plugin default values with site-specific customizations
+ */
+function my_ogp_filter( $meta_tags ) {
+    $meta_tags['http://ogp.me/ns#image'][] = array(
+        'url' => 'http://s.wordpress.org/about/images/logos/wordpress-logo-notext-rgb.png',
+        'type' => 'image/png',
+        'width' => 500,
+        'height' => 500
+    );
+    return $meta_tags;
+}
+add_filter( 'fb_meta_tags', 'my_ogp_filter' );
