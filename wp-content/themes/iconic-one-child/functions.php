df93658942e7d6b8d9c5ca4c0ca1d9f3e687070e
Derek Tu <derektu123@gmail.com>
2015-06-01 19:27:33 +0800

    test: add fallback image for Facebook post

--- wp-content/themes/iconic-one-child/functions.php
+++ wp-content/themes/iconic-one-child/functions.php
@@ -23,3 +23,19 @@ function custom_excerpt_length( $length ) {
 add_filter( 'excerpt_length', 'custom_excerpt_length' );
 
 
+/**
+ * Add site-specific Open Graph protocol customizations to the Facebook plugin
+ *
+ * @param array $meta_tags an associative array of Open Graph protocol properties with keys expressed as full IRIs
+ * @return array plugin default values with site-specific customizations
+ */
+function my_ogp_filter( $meta_tags ) {
+    $meta_tags['http://ogp.me/ns#image'][] = array(
+        'url' => 'http://s.wordpress.org/about/images/logos/wordpress-logo-notext-rgb.png',
+        'type' => 'image/png',
+        'width' => 500,
+        'height' => 500
+    );
+    return $meta_tags;
+}
+add_filter( 'fb_meta_tags', 'my_ogp_filter' );
