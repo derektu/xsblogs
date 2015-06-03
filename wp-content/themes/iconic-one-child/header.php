cce976163d7ba50040ee294949df6dda7f845187
Derek Tu <derektu123@gmail.com>
2015-06-03 17:53:16 +0800

    讓fb like時不會跳出popup的comment box

--- wp-content/themes/iconic-one-child/header.php
+++ wp-content/themes/iconic-one-child/header.php
@@ -29,6 +29,11 @@
     <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
     <![endif]-->
     <?php wp_head(); ?>
+    <style>
+		.fb-like {
+			overflow: hidden !important;
+		}
+    </style>
 </head>
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
